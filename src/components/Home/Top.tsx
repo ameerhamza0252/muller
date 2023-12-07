c6f87bd8145e81ca189402046001a33cb3b8cb53
fahadsalamatrandhawa <dreamtwister61814@gmail.com>
2023-12-07 19:35:29 +0500

    fixed css

--- src/components/Home/Top.tsx
+++ src/components/Home/Top.tsx
@@ -1,26 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BiRightArrowAlt } from "react-icons/bi";
+import Pagelink from "../link";
 
 export default function top({blok}:{blok:any}){
   //console.log(blok) 
     return(
-      <div className={` flex flex-col w-full h-screen bg-cover `} style={{backgroundImage:`url(${blok.image.filename})`}} >
-          <div className=' relative flex flex-col h-[226px] w-[750px] top-[327px] left-[31px]'>
-            <text className=' text-[80px] leading-[84px]'>
-            {blok.Title}
-            </text>
-            <text className=" mt-[36px]">
-            {blok.description}
-            </text>
-            <div className='flex justify-between items-baseline h-[38px] w-[353px] text-[21px] font-medium border-b-[1px] border-white pb-1'>
-              <text>
-                Get info
-              </text>
-              <BiRightArrowAlt/>
-            </div>
+      <div className={` flex flex-col w-full min-h-screen bg-cover justify-end `} style={{backgroundImage:`url(${blok.image.filename})`}} >
+          <div className=' flex flex-col min-h-[226px] w-[750px] top-[327px] left-[31px]'>
+            <text className=' text-[80px] leading-[84px]'>{blok.Title}</text>
+            <text className=" mt-[36px] overflow-hidden ">{blok.description}</text>
+            <Pagelink variant="white" />
           </div>     
-          <Link href="#solutuions" className='relative top-[705px] self-center ' ><Image src="/Icon/down.svg" alt='scroll' width={50} height={50} /></Link>
+          <Link href="#solutuions" className=' self-center justify-self-end ' ><Image src="/Icon/down.svg" alt='scroll' className=" self-end" width={50} height={50} /></Link>
     </div>
     )
 }
--- src/components/link.tsx
+++ src/components/link.tsx
@@ -4,8 +4,8 @@ import { BiRightArrowAlt } from "react-icons/bi"
 export default function Pagelink({text,url,variant="white"}:{text?:string,url?:string,variant?:string}){
   //console.log(variant)
     return(
-        <div className={`flex justify-between items-baseline h-[38px] w-[353px] text-[21px] font-medium border-b-[1px] ${variant=='black'?'border-B-Yellow text-B-Yellow':'border-brand text-brand'} pb-1 `}>
-        <text className={`${variant=='black'?'text-white':'text-black'}`}>
+        <div className={`flex justify-between items-baseline h-[38px] w-[353px] text-[21px] font-medium border-b-[1px] ${variant=='yellow'?'border-B-Yellow text-white':variant=='green'?'border-brand text-black':'text-white border-white'} pb-1 `}>
+        <text>
           {text?text:'Lore Ipsum'}
         </text>
         <BiRightArrowAlt />
