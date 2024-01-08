442f9001591ff1e7d63c6b983172ea0023a1a6f8
fahadsalamatrandhawa <dreamtwister61814@gmail.com>
2024-01-09 01:03:35 +0500

    adjusted width

--- src/components/CommonComponents/ItemCard.tsx
+++ src/components/CommonComponents/ItemCard.tsx
@@ -13,15 +13,15 @@ export default function ItemsCard({blok,buttontext,link_variant,divider_color}:{
     return(
       <>
         <Skeleton isLoaded={!isLoading} fitContent={true}  > 
-        <div className=' self-center flex flex-col w-[300px] md:w-[450px] lg:w-[700px] xl:w-[800px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch px-[10px] gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
+        <div className=' flex flex-col  w-full md:w-[330px] lg:w-[550px] xl:w-[900px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
           
           </div>
       </Skeleton>
       <Skeleton isLoaded={!isLoading} fitContent={true}  > 
-      <div className=' self-center flex flex-col w-[300px] md:w-[450px] lg:w-[700px] xl:w-[800px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch px-[10px] gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
-      
-        </div>
-    </Skeleton>
+        <div className=' flex flex-col  w-full md:w-[330px] lg:w-[550px] xl:w-[900px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
+          
+          </div>
+      </Skeleton>
       </>
     )
   }
@@ -31,11 +31,9 @@ export default function ItemsCard({blok,buttontext,link_variant,divider_color}:{
           {
             data&&data.map((blok:any)=>{
               const stop=blok.content.blocks.filter((b:any)=>b.component=="stop")[0]
-              //const info=blok.content.blocks.filter((b:any)=>b.component=="info")
-              //console.log(stop)
               return(
                 <Skeleton isLoaded={!isLoading} fitContent={true}  > 
-                    <div className=' flex flex-col  w-full md:w-[400px] lg:w-[550px] xl:w-[800px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
+                    <div className=' flex flex-col  w-full md:w-[330px] lg:w-[550px] xl:w-[900px] min-h-[400px] md:min-h-[600px] lg:h-[936px] xl:h-auto justify-stretch gap-[20px] py-[18px] xl:py-[30px]' key={blok.uuid} >
                     <div className=' relative w-full min-h-[358px] lg:min-h-[450px]'>
                         <Image src={stop.image.filename} placeholder='empty' objectFit='cover' alt={stop.image.alt} fill />
                     </div>
